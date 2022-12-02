b2ab90343822ec974a5d358a66f09a4d8afcd8ec
박서연 <sypark@reflexion.co.kr>
2022-12-02 17:15:49 +0900

    react app

---
+++ react-app/practice-react/src/App.js
@@ -0,0 +1,12 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome back!!!!</h1>
+      <Button text={'dldldl'} />
+    </div>
+  );
+}
+
+export default App;
