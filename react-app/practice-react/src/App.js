0463b61ab92cee0071264fc56570b2e5b8d5359e
박서연 <sypark@reflexion.co.kr>
2022-12-05 17:43:49 +0900

    useEffect 학습

--- react-app/practice-react/src/App.js
+++ react-app/practice-react/src/App.js
@@ -1,13 +1,22 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import {useState , useEffect} from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log('항상 실행');
+  useEffect(() => {
+    console.log('한번만 실행');
+  }, []);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!!</h1>
-      <Button text={'dldldl'} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
+
 
 export default App;
