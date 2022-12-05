34a254726439af2d10a17cecd92c282975a2195f
박서연 <sypark@reflexion.co.kr>
2022-12-05 17:57:17 +0900

    useEffect 학습2

--- react-app/practice-react/src/App.js
+++ react-app/practice-react/src/App.js
@@ -4,14 +4,30 @@ import {useState , useEffect} from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log('항상 실행');
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
     console.log('한번만 실행');
   }, []);
 
+  useEffect(() => {
+    if(keyword !== '' && keyword.length > 5) {
+      console.log('키워드가 바뀌면 실행 ' + keyword);
+    }
+  },[keyword])
+  useEffect(() => {
+      console.log('버튼누를때 실행');
+  },[counter]) // 배열안에는 여러개 넣을수 있음
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
