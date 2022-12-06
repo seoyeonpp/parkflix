ffe4ee8fbcd371dd252e2025cf2a113e36e0bdc1
박서연 <sypark@reflexion.co.kr>
2022-12-06 16:50:34 +0900

    cleanup 함수 학습

--- react-app/practice-react/src/App.js
+++ react-app/practice-react/src/App.js
@@ -2,36 +2,24 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import {useState , useEffect} from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log('created :)');
+    // cleanup function : 컴포넌트가 없어질때 호출
+    return () => console.log('destroyed :(');
+  }, [])
+  return <h1>Hello</h1>
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log('한번만 실행');
-  }, []);
-
-  useEffect(() => {
-    if(keyword !== '' && keyword.length > 5) {
-      console.log('키워드가 바뀌면 실행 ' + keyword);
-    }
-  },[keyword])
-  useEffect(() => {
-      console.log('버튼누를때 실행');
-  },[counter]) // 배열안에는 여러개 넣을수 있음
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
     </div>
-  );
+  )
 }
 
 
