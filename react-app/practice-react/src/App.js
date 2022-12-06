caa0ff570510a3ddc3246167ad64a0b0017b245d
박서연 <sypark@reflexion.co.kr>
2022-12-06 17:26:26 +0900

    todo

--- react-app/practice-react/src/App.js
+++ react-app/practice-react/src/App.js
@@ -2,22 +2,31 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import {useState , useEffect} from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log('created :)');
-    // cleanup function : 컴포넌트가 없어질때 호출
-    return () => console.log('destroyed :(');
-  }, [])
-  return <h1>Hello</h1>
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === "") {
+      return;
+    }
+    setTodos((currentArray) => [toDo,...currentArray]);
+    setTodo("");
+  };
+  console.log(toDos)
   return (
     <div>
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-      {showing ? <Hello /> : null}
+      <h1>My Todos</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="to do list"
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   )
 }
